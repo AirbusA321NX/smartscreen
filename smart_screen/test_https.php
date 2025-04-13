be779fd63aba3ac22533028cbcc8285c411a6da3
Shivam Jindal <136959585+AirbusA321NX@users.noreply.github.com>
2025-04-13 13:31:48 +0530

    Delete smart_screen directory

--- smart_screen/test_https.php
+++
@@ -1,8 +0,0 @@
-<?php
-$response = file_get_contents("https://www.google.com");
-if ($response === false) {
-    echo "❌ HTTPS requests are not working in PHP!";
-} else {
-    echo "✅ HTTPS requests are working!";
-}
-?>
